40fb0ee3c354f168bd7715a645f1120c0ca49752
AhGang <474494594@qq.com>
2019-07-12 15:26:03 +0800

    commit test about Fizz

--- src/main/java/FizzBuzz/FizzBuzz.java
+++ src/main/java/FizzBuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package FizzBuzz;
 
 public class FizzBuzz {
     public String convert(int number){
+        if(number % 3 == 0){
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
     @Test
-    void should_get_1_() {
+    void should_get_1_Given_1() {
         //Given
         FizzBuzz fizzBuzz = new FizzBuzz();
         int number = 1;
@@ -14,7 +14,7 @@ public class FizzBuzzTest {
         Assertions.assertEquals("1",result);
     }
     @Test
-    void should_get_2_() {
+    void should_get_2_Given_2() {
         //Given
         FizzBuzz fizzBuzz = new FizzBuzz();
         int number = 2;
@@ -22,5 +22,25 @@ public class FizzBuzzTest {
         String result = fizzBuzz.convert(number);
         //Then
         Assertions.assertEquals("2",result);
+    }
+    @Test
+    void should_get_Fizz_Given_3() {
+        //Given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int number = 3;
+        //When
+        String result = fizzBuzz.convert(number);
+        //Then
+        Assertions.assertEquals("Fizz",result);
+    }
+    @Test
+    void should_get_Fizz_Given_6() {
+        //Given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        int number = 6;
+        //When
+        String result = fizzBuzz.convert(number);
+        //Then
+        Assertions.assertEquals("Fizz",result);
     }
 }
